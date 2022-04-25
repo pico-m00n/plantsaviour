4f6b553c45e1c605cef5d8491b74f936275b310e
pico-m00n <veruzu36@gmail.com>
2022-04-25 07:03:19 +0000

    非同期通信２

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   end
 
   resources :end_users, only: [:show, :edit, :update]
-  resources :questions, shallow: true  d
+  resources :questions, shallow: true  do
     resource :bookmarks, only: [:create, :destroy]
     resources :answers, shallow: true,  only: [:new, :create] do
       resources :reactions, only: [:new, :create]
